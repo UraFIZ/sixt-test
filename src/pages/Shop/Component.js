217fa6f6da6309860eb9ed8f6abde83dc694bffc
UraFIZ <kratser88@gmail.com>
2021-02-11 09:06:34 +0200

    create tilt and header component

--- src/pages/Shop/Component.js
+++ src/pages/Shop/Component.js
@@ -1,22 +1,28 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-
+import Tilt from 'src/components/Tilt';
 import Card from 'src/components/Card';
+import Header from 'src/components/Header';
 import { selectCollections } from 'src/redux/shop/selectors';
 
 const CarStore = ({ collections }) => {
   return (
-    <div className="container">
-      {Object.values(collections).map((item) => (
-        <Card
-          key={item.id}
-          name={item.carGroupInfo.modelExample.name}
-          price="price"
-          image={item.carGroupInfo.modelExample.imageUrl}
-        />
-      ))}
-    </div>
+    <>
+      <Header />
+      <main className="container">
+        {Object.values(collections).map((item) => (
+          <Tilt>
+            <Card
+              key={item.id}
+              name={item.carGroupInfo.modelExample.name}
+              price="price"
+              image={item.carGroupInfo.modelExample.imageUrl}
+            />
+          </Tilt>
+        ))}
+      </main>
+    </>
   );
 };
 
